36ed156debe8f475cdcf68731ccdf9004ea6c7b8
Ubuntu <ubuntu@ip-172-31-9-132.ec2.internal>
2023-02-19 12:06:58 +0000

    initial commit

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,22 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=naittou/ml-api
+echo $PASSWORD | docker login -u  naittou --password-stdin
+docker image tag ml-api $dockerpath
+docker push $dockerpath
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-ml-api\
+    --image=$dockerpath\
+    --port=80 --labels app=udacity-ml-api
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods 
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward udacity-ml-api 8000:80
 
